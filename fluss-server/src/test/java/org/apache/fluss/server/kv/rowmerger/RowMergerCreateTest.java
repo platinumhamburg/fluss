1b08de3fcc5fea17525dc946fb51bc8ecae195d7
ocean.wy <ocean.wy@alibaba-inc.com>
2025-12-31 19:14:09 +0800

    fix unit test

--- fluss-server/src/test/java/org/apache/fluss/server/kv/rowmerger/RowMergerCreateTest.java
+++ fluss-server/src/test/java/org/apache/fluss/server/kv/rowmerger/RowMergerCreateTest.java
@@ -76,6 +76,9 @@ class RowMergerCreateTest {
 
         assertThat(merger).isInstanceOf(AggregateRowMerger.class);
 
+        // Configure target schema before merge
+        merger.configureTargetColumns(null, SCHEMA_ID, schema);
+
         // Verify max aggregation
         BinaryRow row1 = compactedRow(schema.getRowType(), new Object[] {1, 10L});
         BinaryRow row2 = compactedRow(schema.getRowType(), new Object[] {1, 20L});
