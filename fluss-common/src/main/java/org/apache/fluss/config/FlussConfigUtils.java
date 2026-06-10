b336f2eb8502598c64bae9852ece15358a4adfee
Liurnly <masterwangzx@gmail.com>
2026-06-10 10:21:38 +0800

    [server] Allow altering datalake auto-compaction before lake enabled (#3458)

--- fluss-common/src/main/java/org/apache/fluss/config/FlussConfigUtils.java
+++ fluss-common/src/main/java/org/apache/fluss/config/FlussConfigUtils.java
@@ -48,6 +48,7 @@ public class FlussConfigUtils {
                 Arrays.asList(
                         ConfigOptions.TABLE_DATALAKE_ENABLED.key(),
                         ConfigOptions.TABLE_DATALAKE_FRESHNESS.key(),
+                        ConfigOptions.TABLE_DATALAKE_AUTO_COMPACTION.key(),
                         ConfigOptions.TABLE_TIERED_LOG_LOCAL_SEGMENTS.key(),
                         ConfigOptions.TABLE_AUTO_PARTITION_NUM_RETENTION.key(),
                         ConfigOptions.TABLE_AUTO_PARTITION_NUM_PRECREATE.key(),
