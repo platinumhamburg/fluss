c1df5a65d753fab574760237225c6c3c1a2e4607
白鵺 <ocean.wy@alibaba-inc.com>
2026-07-11 23:44:15 +0800

    [flink] Fix scope progress test header

--- fluss-flink/fluss-flink-common/src/test/java/org/apache/fluss/flink/action/orphan/job/ScopeProgressTrackerTest.java
+++ fluss-flink/fluss-flink-common/src/test/java/org/apache/fluss/flink/action/orphan/job/ScopeProgressTrackerTest.java
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.apache.fluss.flink.action.orphan.job;
 
 import org.junit.jupiter.api.Test;
